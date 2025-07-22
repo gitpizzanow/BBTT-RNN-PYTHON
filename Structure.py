d389b46060c9f82505692b22b34ae622eee9c41b
Lyudmira Segfaultovna <sahraouitarikziad@gmail.com>
2025-07-22 05:32:36 -1100

    Create Structure.py

---
+++ Structure.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class RNN :
+    def __init__(this, 
+        max_timestep,
+        input_size  = 1,
+        hidden_size = 1, #2,3,4....
+        output_size = 1
+        ):
+        this.max_timestep = max_timestep
+        this.input_size = input_size
+        this.hidden_size = hidden_size
+        this.output_size = output_size
+
+        this.wxh = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
+        this.whh = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
+        this.why = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
+
+        this.bxh = np.zeros((hidden_size, 1))
+        this.by  = np.zeros((output_size, 1))
+
+        this.h      = [np.zeros((hidden_size, 1)) for _ in range(max_timestep + 1)]
+        this.h_row  = [np.zeros((hidden_size, 1)) for _ in range(max_timestep)]
+        this.y_pred = [np.zeros((output_size, 1)) for _ in range(max_timestep)]
+        this.loss   = np.zeros(max_timestep)
+
+
