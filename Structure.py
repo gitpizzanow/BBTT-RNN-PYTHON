89378e7bdbf02d7e6530a6ffe3837ab5128dcbef
Lyudmira Segfaultovna <sahraouitarikziad@gmail.com>
2025-07-22 08:13:49 -1100

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -4,7 +4,7 @@ class RNN :
     def __init__(this, 
         max_timestep,
         input_size  = 1,
-        hidden_size = 1, #2,3,4....
+        hidden_size = 3, 
         output_size = 1
         ):
         this.max_timestep = max_timestep
@@ -12,9 +12,9 @@ class RNN :
         this.hidden_size = hidden_size
         this.output_size = output_size
 
-        this.wxh = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
-        this.whh = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
-        this.why = np.random.uniform(-1,1,(hidden_size * input_size)) * 0.1
+        this.wxh = np.random.uniform(-1,1,(hidden_size , input_size)) * 0.1
+        this.whh = np.random.uniform(-1,1,(hidden_size , hidden_size)) * 0.1
+        this.why = np.random.uniform(-1,1,(output_size , hidden_size)) * 0.1
 
         this.bxh = np.zeros((hidden_size, 1))
         this.by  = np.zeros((output_size, 1))
